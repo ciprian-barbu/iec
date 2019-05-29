6a5afa9d9cc2d8643ff8d3c939051f08af0e61e7
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2019-05-29 18:08:05 +0800

    Remove parameter of "cp" command
    The parameter may block the scripts when deploying
    k8s environments.
    
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>
    Change-Id: I91e7a927d17ae15fd51c45047de091f4a3f4877a

--- src/foundation/scripts/k8s_master.sh
+++ src/foundation/scripts/k8s_master.sh
@@ -25,7 +25,7 @@ if ! kubectl get nodes; then
   fi
 
   mkdir -p "${HOME}/.kube"
-  sudo cp -i /etc/kubernetes/admin.conf "${HOME}/.kube/config"
+  sudo cp /etc/kubernetes/admin.conf "${HOME}/.kube/config"
   sudo chown "$(id -u)":"$(id -g)" "${HOME}/.kube/config"
 
   sleep 5
