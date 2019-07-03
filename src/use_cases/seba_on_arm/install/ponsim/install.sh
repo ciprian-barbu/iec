15dc3de0d830f1cc83b1445560e759bc732fd646
Ciprian Barbu <ciprian.barbu@enea.com>
2019-07-03 17:10:46 +0300

    More work for ponsim install.sh

--- src/use_cases/seba_on_arm/install/ponsim/install.sh
+++ src/use_cases/seba_on_arm/install/ponsim/install.sh
@@ -14,15 +14,38 @@ export WORKSPACE={HOME}
 
 mkdir -p ${M}
 mkdir -p {$WORKSPACE}/cord/test
+# Update helm-charts submdule needed later
+git submodule update --init ${basepath}/../../src_repo/helm-charts
 
 cd ${WORKSPACE} && git clone https://github.com/opencord/automation-tools.git
 cd ${WORKSPACE}/automation-tools && git checkout ${AUTO_TOOLS_VER}
 
 # Fake the setup phase so that portcheck.sh is not called
 # also install some required packages
-touch ${M}/setup
 apt install -y httpie jq software-properties-common
+touch ${M}/setup
 
-# prepare helm-charts repo
+# Skip helm installation if it already exists and fake /usr/local/bin/helm
+if xhelm=$(which helm)
+then
+  if [ "${xhelm}" != /usr/local/bin/helm ]
+  then
+     "helm is installed at $x; symlinking to /usr/local/bin/helm"
+     mkdir -p /usr/local/bin/
+     ln -s /usr/local/bin/helm "${xhelm}"
+else
+  echo "helm is not installed"
+fi
 
-milestones="kubeadm helm-init"
+# Faking helm-charts repo clone to our own git submodule
+ln -s "${basepath}/../../src_repo/helm-charts ${WORKSPACE}/cord/helm-charts"
+
+# Fake SiaB components setup since they are already installed
+milestones="kubeadm helm-init kafka kafka-running etcd-operator-ready voltha \
+            voltha-running nem onos siab"
+
+for m in "${milestones}";
+do
+  echo "Faking SiaB milestone ${M}/${m}"
+  touch "${M}/${m}"
+done
