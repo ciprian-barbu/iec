1aabb2b115c896fcbcaf8842c199b35ff1144cb8
Ciprian Barbu <ciprian.barbu@enea.com>
2020-03-24 04:32:52 +0200

    Cleanup and alignment with automation-tools env.sh
    Signed-off-by: Ciprian Barbu <ciprian.barbu@enea.com>
    Change-Id: I8dd86f1dc875e6638b89d8f1e7cc4cf8761493e2

--- src/use_cases/seba_on_arm/test/ponsim/install.sh
+++ src/use_cases/seba_on_arm/test/ponsim/install.sh
@@ -5,13 +5,14 @@ set -ex
 
 basepath=$(cd "$(dirname "$0")"; pwd)
 
-# Using opencord automation-tools from the cord-6.1 maintenance branch
-AUTO_TOOLS_GIT="https://github.com/iecedge/automation-tools.git"
-AUTO_TOOLS_VER=${AUTO_TOOLS_VER:-cord-7.0-arm64}
-
 export M=/tmp/milestones
 export SEBAVALUE=
 export WORKSPACE=${HOME}
+
+# Using opencord automation-tools from the cord-6.1 maintenance branch
+AUTO_TOOLS="${WORKSPACE}/automation-tools"
+AUTO_TOOLS_REPO="https://github.com/iecedge/automation-tools.git"
+AUTO_TOOLS_REV=${AUTO_TOOLS_VER:-cord-7.0-arm64}
 
 rm -rf "${M}"
 mkdir -p "${M}" "${WORKSPACE}/cord/test"
@@ -20,18 +21,16 @@ mkdir -p "${M}" "${WORKSPACE}/cord/test"
 # ignore subproject commit and use latest remote version
 git submodule update --init --remote "${basepath}/../../src_repo/helm-charts"
 
-cd "${WORKSPACE}"
-test -d automation-tools || git clone "${AUTO_TOOLS_GIT}"
-(cd "${WORKSPACE}/automation-tools" && git checkout "${AUTO_TOOLS_VER}")
+test -d "${AUTO_TOOLS}" || git clone "${AUTO_TOOLS_REPO}" "${AUTO_TOOLS}"
+(cd "${AUTO_TOOLS}"; git checkout "${AUTO_TOOLS_REV}")
 
 # Faking helm-charts repo clone to our own git submodule if not already there
 CHARTS="${WORKSPACE}/cord/helm-charts"
 test -d "${CHARTS}" || test -L "${CHARTS}" || \
     ln -s "${basepath}/../../src_repo/helm-charts" "${CHARTS}"
 
-cd "${WORKSPACE}/automation-tools/seba-in-a-box"
+cd "${AUTO_TOOLS}/seba-in-a-box"
 . env.sh
 
 # Now calling make, to install PONSim
 make stable
-
