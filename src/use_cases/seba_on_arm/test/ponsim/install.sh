77463dce47d4eb4cebc5b5cc5ea3ff49c773b562
Ciprian Barbu <ciprian.barbu@enea.com>
2020-07-16 03:38:18 +0300

    Compatibility for older git versions
    Signed-off-by: Ciprian Barbu <ciprian.barbu@enea.com>
    Change-Id: I52e6ffcbceb8277010b3b7d690d8f234ed791170

--- src/use_cases/seba_on_arm/test/ponsim/install.sh
+++ src/use_cases/seba_on_arm/test/ponsim/install.sh
@@ -5,7 +5,7 @@ set -ex
 
 basepath="$(cd "$(dirname "$(readlink -f "$0")")"; pwd)"
 
-IEC_PATH="$(readlink -f "$(git -C "${basepath}" rev-parse --show-toplevel)")"
+IEC_PATH="$(readlink -f "$(cd "${basepath}"; git rev-parse --show-toplevel)")"
 HELM_CHARTS_PATH="src/use_cases/seba_on_arm/src_repo/helm-charts"
 HELM_CHARTS_REV_IEC="cord-7.0-arm64"
 HELM_CHARTS_REV_REC="cord-7.0-arm64-rec"
@@ -44,11 +44,11 @@ mkdir -p "${M}" "${WORKSPACE}/cord/test"
 
 # Update helm-charts submdule needed later
 # ignore subproject commit and use latest remote version
-git -C "${IEC_PATH}" submodule update --init --remote "${HELM_CHARTS_PATH}"
-git -C "${IEC_PATH}/${HELM_CHARTS_PATH}" checkout "${HELM_CHARTS_REV}"
+(cd "${IEC_PATH}"; git submodule update --init --remote "${HELM_CHARTS_PATH}")
+(cd "${IEC_PATH}/${HELM_CHARTS_PATH}"; git checkout "${HELM_CHARTS_REV}")
 
 test -d "${AUTO_TOOLS}" || git clone "${AUTO_TOOLS_REPO}" "${AUTO_TOOLS}"
-git -C "${AUTO_TOOLS}" checkout "${AUTO_TOOLS_REV}"
+(cd "${AUTO_TOOLS}"; git checkout "${AUTO_TOOLS_REV}")
 
 # Faking helm-charts repo clone to our own git submodule if not already there
 CHARTS="${WORKSPACE}/cord/helm-charts"
